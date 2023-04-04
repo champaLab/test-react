933af7d560e78e2633bc75a61eb48ad6c33892c7
sonephet <sonephet.dev@gmail.com>
2023-04-04 12:02:52 +0700

    call api tracking

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
 
-const socket = io.connect(window.location.hostname);
+const socket = io.connect('https://socket-inky.vercel.app');
 
 function App() {
   //Room State
